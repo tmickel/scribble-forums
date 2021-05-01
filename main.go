e8ee0b872d384815082782c74350b8ff3de9f6f9
Big Fun Cloud TODO <tim@tmickel.com>
2021-05-01 23:12:36 +0000

    Automated from Big Fun Cloud: M  main.go

--- main.go
+++ main.go
@@ -11,5 +11,5 @@ func main() {
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello from scribble-forums! We are temporarily down because I have to add the code to Git", r.URL.Path[1:])
+	fmt.Fprintf(w, "Hello from scribble-forums! We are temporarily down because I have to add the code to Git")
 }
