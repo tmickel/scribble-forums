7113d3601bdaffdfbfeb886c9fdcd1e2c5ee3d39
Tim Mickel <tim@tmickel.com>
2021-06-25 14:35:22 +0000

    undo innocuous

--- flash.go
+++ flash.go
@@ -2,7 +2,6 @@ package main
 
 import "net/http"
 
-// innocuous change
 func setFlash(w http.ResponseWriter, message string, style string) {
 	http.SetCookie(w, &http.Cookie{
 		Name:     "flash_text",
